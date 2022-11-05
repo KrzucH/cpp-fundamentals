9dabfddbf03fb59eeabc80b4bd856e4840f99cfe
Krzysiek Nowak <krzynowak93@gmail.com>
2022-11-05 18:47:38 +0100

    Homework ADD EVEN jazda

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -3,6 +3,14 @@
 
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    // Below return is only to make this function compile nodsds
+    std::vector<int> vec;
+    int a {0};
+    for(auto i = 0; i < numbers.size(); i++) {
+        if ((numbers[i] % 2) == 0){
+            a += numbers [i];
+        }
+    }
+                
+    return a;
 }
