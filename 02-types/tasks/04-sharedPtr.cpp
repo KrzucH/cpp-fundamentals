572af10e10112d7f6dc67a7a4ea531623fb45e8f
Krzysiek Nowak <krzynowak93@gmail.com>
2022-11-07 15:02:47 +0100

    Zadanka#2poprawka

--- 02-types/tasks/04-sharedPtr.cpp
+++ 02-types/tasks/04-sharedPtr.cpp
@@ -4,13 +4,19 @@
 // TODO: Implement foo()
 // It should take shared_ptr to int and assign value 20 to the pointed int.
 // It should also display the value of this int and the number of how many pointers are pointing to it - use `use_count()`.
-// Display the same information in main() before and after calling foo()
+// Display the same information in main() before and after calling foo(
+int foo(std::shared_ptr<int> num) {
+    *num = 20;
+    std::cout << "Value of shared_ pointer " << *num << " | " << num.use_count() << std::endl; 
+    return *num;
+}
 
 int main() {
     std::shared_ptr<int> number = std::make_shared<int>(10);
     // display the value under number pointer and use_count() of it
+    std::cout << "Value of shared_ pointer " << *number << " | " << number.use_count() << std::endl; 
     foo(number);
     // display the value under number pointer and use_count() of it
-
+    std::cout << "Value of shared_ pointer " << *number << " | " << number.use_count() << std::endl;
     return 0;
 }
--- 02-types/tasks/05-calculateProduct.cpp
+++ 02-types/tasks/05-calculateProduct.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <memory>
 
 // TODO: Implement calculateProduct()
 // It should take 2 int values and return their product as a shared_ptr.
-// Additionally, check how many owners are there.
+// Additionally, check how many owners are theredsds
+std::shared_ptr<int> calculateProduct(int a, int b) {
+    std::shared_ptr<int> add = std::make_shared<int> (a * b);
+    std::cout << "num: " << *add << " | owners: " << add.use_count()<<std::endl;
+    return add;
+}
 
 int main() {
     auto number = calculateProduct(10, 20);
